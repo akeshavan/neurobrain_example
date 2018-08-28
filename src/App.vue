71947076cd6fcb751fada5e3124205e9bc67d269
akeshavan <keshavan@berkeley.edu>
2018-08-28 09:35:01 -0700

    fix: changed API a bit, it can now show all 3 views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <div id="view1"></div>
+
+    <div class="views">
+      <div id="view1"></div>
+      <div class="miniviews">
+        <div id="view2"></div>
+        <div id="view3"></div>
+      </div>
+    </div>
     <!--<router-view/>-->
   </div>
 </template>
@@ -16,16 +23,31 @@ export default {
     window.viewer = viewer;
     console.log('initialized viewer');
 
-    const view1 = new neurobrain.View('view1');
-    window.view = view1;
+    const view1 = new neurobrain.View('view1', 'axial');
+    window.view1 = view1;
     console.log('initialized view');
 
+    const view2 = new neurobrain.View('view2', 'sagittal');
+    window.view2 = view2;
+    console.log('initialized view2');
 
-    const file = 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/adi_brain/adi_brain.nii.gz';
+    const view3 = new neurobrain.View('view3', 'coronal');
+    window.view3 = view3;
+
+    const file = 'https://dxugxjm290185.cloudfront.net/hbn/sub-NDARKH741PL8/anat/sub-NDARKH741PL8_T1w.nii.gz?dl=0'; 
+    // 'https://cdn.rawgit.com/FNNDSC/data/master/nifti/adi_brain/adi_brain.nii.gz';
     viewer.addView(view1);
-    console.log('added view');
-    viewer.addLayer(file, view1);
-    console.log('added layer');
+    viewer.addView(view2);
+    viewer.addView(view3);
+    console.log('added view 1,2,3');
+    viewer.addData(file, 'base_image')
+      .then((series) => {
+        viewer.addLayer(series, 'view1');
+        viewer.addLayer(series, 'view2');
+        viewer.addLayer(series, 'view3');
+        console.log('added layer 1,2,3');
+      });
+
   },
 };
 
@@ -41,8 +63,29 @@ export default {
   margin-top: 60px;
 }
 
+.views {
+  display: flex;
+}
+
+.miniviews {
+  display: block;
+}
+
 #view1 {
   width: 500px;
   height: 500px;
+  display: inherit;
+}
+
+#view2 {
+  width: 250px;
+  height: 250px;
+  display: inherit;
+}
+
+#view3 {
+  width: 250px;
+  height: 250px;
+  display: inherit;
 }
 </style>
